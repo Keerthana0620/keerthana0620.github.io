3957f745e5cb3caa169f588907fb57b372e200d9
Keerthana0620 <keerthana.aravindhan@gwmail.gwu.edu>
2025-09-28 20:58:26 -0700

    portfolio creation

---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js'
+
+var options = {
+  strings: ["I'm a AI/ML Engineer", "I'm a Software Engineer", "Master's Graduate in Data Science"],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true,
+}
+
+var typed = new Typed('#headline', options)
